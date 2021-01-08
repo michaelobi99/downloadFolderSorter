69830ef2ae49d9fd99e471f30bae68a369ff0d7c
michael <michaelobi54@gmail.com>
2021-01-09 00:29:18 +0100

    switched from using condition variables to std::async

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,10 @@
 #include <string>
 #include <string_view>
 #include <condition_variable>
-#include <queue>
+#include <future>
 #include <map>
 #include <limits>
 #include <fstream>
-#include "FileQueue.h"
 //.........................................................................................................................
 //optional compilation..
 //this part of the code works only on windows
@@ -30,6 +29,8 @@ void hide(bool hide) {
 
 namespace fs = std::filesystem;
 using namespace std::literals::chrono_literals;
+std::condition_variable condition;
+std::mutex mut;
 
 void monitorKeyboard() {
 	hide(0);
@@ -46,60 +47,72 @@ void monitorKeyboard() {
 	}
 	hide(1);
 }
-void checkForNewFilesInDownloadFolder(FileQueue& queue, std::condition_variable& queueFilled) {
-	fs::path folder = R"(C:\Users\HP\Downloads)";
-	while (true) {
-		try {
-			if (fs::exists(folder) && fs::is_directory(folder)) {
-				for (auto const& file : fs::directory_iterator(folder, fs::directory_options::skip_permission_denied)) {
-					try {
-						if (fs::is_regular_file(file)) {
-							auto fullPath = file.path().parent_path() / file.path().filename();
-							queue.PushFile(fullPath);
-						}
-					}
-					catch (fs::filesystem_error const& error) {}
-					catch (std::exception const& error) {}
-				}
-
-			}
-		}
-		catch (fs::filesystem_error const& error) {}
-		catch (std::exception const& error) {}
-	}
-	
-}
-
-void moveFilesToFolder(FileQueue& queue, std::condition_variable& queueFilled) {
-	extern std::string getExactFolder();
-	while (true) {
-		std::vector<std::string> fileExtensionsToLookFor{ ".pdf", ".mkv", ".mp4", ".mp3", ".gif", ".png", ".jpg" };
-		std::map<std::string, std::string> folderDestinations{
-			{"video", R"(C:\Users\HP\Videos)"},
-			{"music", R"(C:\Users\HP\Music\music)"},
-			{"picture", R"(C:\Users\HP\Pictures)"},
-			{"book", getExactFolder()}
-		};
-	}
-}
 std::string getExactFolder() {
 	return "";
 }
 
+std::vector<fs::path> listDir() {
+	std::vector<std::string> fileExtensionsToLookFor{ ".pdf", ".mkv", ".mp4", ".mp3", ".gif", ".png", ".jpg" };
+	auto filter = [&fileExtensionsToLookFor](fs::path const& file) {
+		for (auto const& extension : fileExtensionsToLookFor) {
+			if (file.extension().string() == extension)
+				return true;
+		}
+		return false;
+	};
+	fs::path folder = R"(C:\Users\HP\Downloads)";
+	std::vector<fs::path> listOfPaths;
+	listOfPaths.reserve(100);
+	try {
+		if (fs::exists(folder) && fs::is_directory(folder)) {
+			for (auto const& file : fs::directory_iterator(folder, fs::directory_options::skip_permission_denied)) {
+				try {
+					if (fs::is_regular_file(file) && filter(file)) {
+						listOfPaths.emplace_back(file.path().parent_path() / file.path().filename());
+						if (listOfPaths.capacity() == listOfPaths.size())
+							listOfPaths.reserve(50);
+					}
+				}
+				catch (fs::filesystem_error const&) {}
+				catch (std::exception const&) {}
+			}
+		}
+	}
+	catch (fs::filesystem_error const&) {}
+	catch (std::exception const&) {}
+	return listOfPaths;
+}
+
+void moveFilesToFolder(std::vector<fs::path> const& listOfPaths) {
+	std::map<std::string, std::string> folderDestinations{
+		{"video", R"(C:\Users\HP\Videos)"},
+		{"music", R"(C:\Users\HP\Music\music)"},
+		{"picture", R"(C:\Users\HP\Pictures)"},
+		{"book", getExactFolder()}
+	};
+	for (auto elem : listOfPaths) {
+		std::cout << elem.string() << "\n";
+	}
+}
+
+void mainProcess() {
+	while (true) {
+		std::future<std::vector<fs::path>> listOfPaths = std::async(listDir);
+		moveFilesToFolder(listOfPaths.get());
+		std::cout << "\n\n\nGoing again\n\n";
+		std::this_thread::sleep_for(3s);
+	}
+}
+
 int main() {
-	auto queue = FileQueue();
-	std::condition_variable queueFilled;
 	std::cout << "***********************************************************************************\n";
 	std::cout << "*                     Download Folder Sorter v1.0                                 *\n";
 	std::cout << "*    Press Esc key on your keyboard to stop this app running in the backgroung    *\n";
 	std::cout << "*      In 5 seconds this application will start execution in the background       *\n";
 	std::cout << "***********************************************************************************\n";
 	std::this_thread::sleep_for(5s);
-	//std::thread downloadFolderMonitorThread(checkForNewFilesInDownloadFolder, std::ref(queue), std::ref(queueFilled));
-	//std::thread fileMoverThread(moveFilesToFolder, std::ref(queue), std::ref(queueFilled));
-	//downloadFolderMonitorThread.detach();
-	//fileMoverThread.detach();
 	std::cin.exceptions(std::fstream::badbit | std::fstream::failbit);
+	std::thread workerThread(mainProcess);
 	while (true) {
 		std::thread keyboardMonitoringThread(monitorKeyboard);
 		while (true) {
@@ -112,7 +125,7 @@ int main() {
 				else
 					continue;
 			}
-			catch (std::exception const& e) {
+			catch (std::exception const&) {
 				std::cin.clear();
 				std::cin.ignore(1000, '\n');
 				std::cout << "ERROR: Invalid command\n";
@@ -120,4 +133,5 @@ int main() {
 		}
 		keyboardMonitoringThread.join();
 	}
+	workerThread.join();
 }
